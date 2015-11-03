c599a7af620fa0d0db58d9ac49ac79a0363dfd97
Pedro Antonio <petterlx727@gmail.com>
2015-11-03 00:48:54 -0600

    Button listeners were added

--- app/src/main/java/com/silmood/who_is/MainActivity.java
+++ app/src/main/java/com/silmood/who_is/MainActivity.java
@@ -2,12 +2,33 @@ package com.silmood.who_is;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button trueButton = (Button) findViewById(R.id.button_true);
+        Button falseButton = (Button) findViewById(R.id.button_false);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
