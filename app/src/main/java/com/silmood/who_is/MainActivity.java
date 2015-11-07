3a97155f6a628e985973b6e0e422ea8b40598d83
Pedro Antonio <petterlx727@gmail.com>
2015-11-06 22:55:44 -0600

    Logic added to update character

--- app/src/main/java/com/silmood/who_is/MainActivity.java
+++ app/src/main/java/com/silmood/who_is/MainActivity.java
@@ -4,10 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mCharacterImage;
+    private TextView mCharacterName;
+
+    private Character[] mCharacters = new Character[] {
+            new Character(R.string.bruce_name, R.drawable.bruce),
+            new Character(R.string.jim_name, R.drawable.jim),
+            new Character(R.string.nicolas_name, R.drawable.nicolas)
+    };
+
+    private Random mRandom;
+    private int mCurrentIndex = 0;
+    private int mCurrentNameId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +32,42 @@ public class MainActivity extends AppCompatActivity {
 
         Button trueButton = (Button) findViewById(R.id.button_true);
         Button falseButton = (Button) findViewById(R.id.button_false);
+        mCharacterImage = (ImageView) findViewById(R.id.image_character);
+        mCharacterName = (TextView) findViewById(R.id.name_character);
+        mRandom = new Random();
 
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.correct_answer, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, compareNames()? R.string.correct_answer : R.string.incorrect_answer,
+                        Toast.LENGTH_SHORT).show();
+                mCurrentIndex = (mCurrentIndex + 1) % mCharacters.length;
+                updateCharacter();
             }
         });
 
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_answer, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, !compareNames()? R.string.correct_answer : R.string.incorrect_answer,
+                        Toast.LENGTH_SHORT).show();
+                mCurrentIndex = (mCurrentIndex + 1) % mCharacters.length;
+                updateCharacter();
             }
         });
+    }
+
+    private void updateCharacter(){
+        int characterImage = mCharacters[mCurrentIndex].getImageResId();
+        int characterName = mCharacters[mRandom.nextInt(3)].getNameResId();
+
+        mCharacterImage.setImageResource(characterImage);
+        mCharacterName.setText(characterName);
+
+        mCurrentNameId = characterName;
+    }
+
+    private boolean compareNames(){
+        return mCurrentNameId == mCharacters[mCurrentIndex].getNameResId();
     }
 }
