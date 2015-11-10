6158e0d13a884450e8285b727b6878dfe51b7e31
Pedro Antonio <petterlx727@gmail.com>
2015-11-09 18:16:53 -0600

    Activity lifecycle methods were overloaded

--- app/src/main/java/com/silmood/who_is/MainActivity.java
+++ app/src/main/java/com/silmood/who_is/MainActivity.java
@@ -2,6 +2,7 @@ package com.silmood.who_is;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private ImageView mCharacterImage;
     private TextView mCharacterName;
@@ -53,6 +56,35 @@ public class MainActivity extends AppCompatActivity {
         updateCharacter();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
 
     private void updateCharacter(){
         int characterImage = mCharacters[mCurrentIndex].getImageResId();
